77826d4263432670aff1c1170f1136024fa4fde7
Raphael <rapho.ngenga@gmail.com>
2026-06-13 17:14:41 +0300

    Remove API key from code

--- Netlify/Functions/mailchimp.js
+++ Netlify/Functions/mailchimp.js
@@ -1,23 +1,35 @@
-exports.handler = async function (event) {
-    const https = require('https');
-    const MC_API_KEY = '5d60e5a2ee118dbe110e5f6f9ee4a5c2-us8';
-    const MC_DC = 'us8';
-    const endpoint = event.queryStringParameters.endpoint || '';
-    const url = `https://${MC_DC}.api.mailchimp.com/3.0${endpoint}`;
-    const auth = Buffer.from('nexus:' + MC_API_KEY).toString('base64');
+exports.handler = async function(event) {
+  const https = require('https');
 
-    return new Promise((resolve) => {
-        https.get(url, { headers: { 'Authorization': 'Basic ' + auth } }, (res) => {
-            let data = '';
-            res.on('data', chunk => data += chunk);
-            res.on('end', () => resolve({
-                statusCode: 200,
-                headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
-                body: data
-            }));
-        }).on('error', (e) => resolve({
-            statusCode: 500,
-            body: JSON.stringify({ error: e.message })
-        }));
-    });
+  // API key is read securely from Netlify environment variables (not stored in code)
+  const MC_API_KEY = process.env.MAILCHIMP_API_KEY;
+  const MC_DC = process.env.MAILCHIMP_DC || 'us8';
+
+  if (!MC_API_KEY) {
+    return {
+      statusCode: 500,
+      headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
+      body: JSON.stringify({ error: 'MAILCHIMP_API_KEY environment variable not set in Netlify' })
+    };
+  }
+
+  const endpoint = event.queryStringParameters.endpoint || '';
+  const url = `https://${MC_DC}.api.mailchimp.com/3.0${endpoint}`;
+  const auth = Buffer.from('nexus:' + MC_API_KEY).toString('base64');
+
+  return new Promise((resolve) => {
+    https.get(url, { headers: { 'Authorization': 'Basic ' + auth } }, (res) => {
+      let data = '';
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => resolve({
+        statusCode: 200,
+        headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
+        body: data
+      }));
+    }).on('error', (e) => resolve({
+      statusCode: 500,
+      headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
+      body: JSON.stringify({ error: e.message })
+    }));
+  });
 };
